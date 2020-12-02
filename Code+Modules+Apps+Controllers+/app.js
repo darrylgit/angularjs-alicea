108934b4218c31ab91d7514181fbc5350e391587
darrylgit <darrylsmithup@gmail.com>
2020-12-01 20:33:29 -0500

    Inject dependencies

--- Code+Modules+Apps+Controllers+/app.js
+++ Code+Modules+Apps+Controllers+/app.js
@@ -1,10 +1,16 @@
 // MODULE
-var angularApp = angular.module('angularApp', []);
+var angularApp = angular.module('angularApp', ['ngMessages', 'ngResource']);
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', function ($scope) {
-    
-}]);
+angularApp.controller('mainController', [
+  '$scope',
+  '$log',
+  '$filter',
+  function ($scope, $log, $filter, $resource) {
+    $scope.name = 'John';
+    $scope.formattedName = $filter('uppercase')($scope.name);
 
-
-
+    $log.info($scope.name);
+    $log.info($scope.formattedName);
+  }
+]);
