ea799506f972330029a17bcf22bebab9545203d0
darrylgit <darrylsmithup@gmail.com>
2020-12-02 22:27:28 -0500

    Interpolate string in html

--- Code+Scope+And+Interpolation+/Starter/app.js
+++ Code+Scope+And+Interpolation+/Starter/app.js
@@ -1,6 +1,13 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', function($scope) {
-    
-    
-}]);
+myApp.controller('mainController', [
+  '$scope',
+  '$timeout',
+  function ($scope, $timeout) {
+    $scope.name = 'Daniel';
+
+    $timeout(function () {
+      $scope.name = 'Everybody';
+    }, 3000);
+  }
+]);
