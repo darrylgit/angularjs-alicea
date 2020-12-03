89a447b0321e89570e9633d4c205f198ff1917fd
darrylgit <darrylsmithup@gmail.com>
2020-12-02 22:43:21 -0500

    Interpolate with

--- Code+Directives+2+Way+Binding+/Starter/app.js
+++ Code+Directives+2+Way+Binding+/Starter/app.js
@@ -1,7 +1,12 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', function($scope) {
-    
-    
-    
-}]);
+myApp.controller('mainController', [
+  '$scope',
+  '$filter',
+  function ($scope, $filter) {
+    $scope.handle = '';
+    $scope.lowercaseHandle = function () {
+      return $filter('lowercase')($scope.handle);
+    };
+  }
+]);
